f134a366fa04639154a6ee0ee4b6ca205f670591
danieloselu3 <doselu3@gmail.com>
2024-06-12 14:12:35 +0300

    added the orders model

--- AfricaSafarisAnalysis/models/staging/stg_tpch_orders.sql
+++ AfricaSafarisAnalysis/models/staging/stg_tpch_orders.sql
@@ -1,2 +1,7 @@
-SELECT *
+SELECT 
+    o_orderKey as order_key,
+    o_custkey as customer_key,
+    o_orderstatus as status_code,
+    o_totalprice as total_rice,
+    o_orderdate as order_date
 FROM {{ source('tpch', 'orders') }}
